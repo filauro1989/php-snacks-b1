d1f6d1b2af3be46bf78c8764447dcb5e7a5fc367
filauro1989 <filauro1989@gmail.com>
2022-02-04 17:10:05 +0100

    milestone 2

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>    <title>php-snacks-b1</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <link rel="stylesheet" href="css/style.css">   <title>php-snacks-b1</title>
 </head>
 <body>
     
-    <div id="app">
+    <div class="wrapper" id="app">
 
     <div v-for="(auto, index) in autoUsate" :key="index" class="car">
         <h2>
